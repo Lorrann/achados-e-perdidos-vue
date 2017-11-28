ff42303f4d728e2e17ff442f76daed7cef445062
Lorrann Corrêa <lorrann.correa@esx.com.br>
2017-11-27 22:19:23 -0200

    incluindo front

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <nav class="demo">
+      <a href="#/" class="brand">
+        <span>Achados e Perdidos</span>
+      </a>
+
+      <!-- responsive-->
+      <input id="bmenub" type="checkbox" class="show">
+      <label for="bmenub" class="burger pseudo button">menu</label>
+
+      <div class="menu">
+        <a href="#/login" class="pseudo button icon-picture">Log in</a>
+      </div>
+    </nav>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
---
+++ src/components/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div style="padding: 10%;">
+        <div class="card">
+            <header class="login">Login</header>
+            <div>
+                <label>User</label>
+                <input type="text"  placeholder="Ex: user@xpto.com" autofocus/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" placeholder="Ex: ******">
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+}
+</script>
+
+<style>
+    .login{
+        text-align: center;
+    }
+</style>
